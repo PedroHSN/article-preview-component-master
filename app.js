2f2575366e87b5b0f282175fae6589580b980183
PedroHSN <pedrodti2017@gmail.com>
2021-06-30 22:41:47 -0300

    função do botao share

---
+++ app.js
@@ -0,0 +1,7 @@
+const socials = document.querySelector(" .open-share")
+
+const shareBtn = document.getElementById("share")
+
+shareBtn.addEventListener("click", () => {
+    socials.classList.add("clicked")
+})
