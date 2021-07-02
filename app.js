5cb00437c8c4231545746828601e023d4ed48a1d
PedroHSN <pedrodti2017@gmail.com>
2021-07-01 23:18:14 -0300

    remover a aba share

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const socials = document.querySelector(" .open-share")
 
 const shareBtn = document.getElementById("share")
 
+const removeShareBtn = document.querySelector("remove-share")
+
 shareBtn.addEventListener("click", () => {
     socials.classList.add("clicked")
+})
+
+
+removeShareBtn.addEventListener("click", () => {
+    socials.classList.remove("clicked")
 })
